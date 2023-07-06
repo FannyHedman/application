d86943fb6f7d16dc251ec114e33d614bd38e0dda
Fanny <fannyhedmans@gmail.com>
2023-07-06 15:16:00 +0200

    Basic CSS

--- src/components/EmployeePage.vue
+++ src/components/EmployeePage.vue
@@ -1,21 +1,25 @@
 <template>
   <div>
-    <div v-for="employee in paginatedData" :key="employee.id">
-      <img :src="employee.avatar" alt="Avatar"/>
-      <p>{{ employee.first_name }} {{ employee.last_name }}</p>
-      <p>
-        <a :href="'mailto:' + employee.email">{{ employee.email }}</a>
-      </p>
+    <div class="grid-container">
+      <div class="grid-content" v-for="employee in paginatedData" :key="employee.id">
+        <img class="img" :src="employee.avatar" alt="Avatar" />
+        <p>{{ employee.first_name }} {{ employee.last_name }}</p>
+        <p>
+          <a class="link" :href="'mailto:' + employee.email">{{ employee.email }}</a>
+        </p>
+      </div>
     </div>
-
-    <button
-      v-for="pageNumber in totalPages"
-      :key="pageNumber"
-      @click="fetchEmployees(pageNumber)"
-      :disabled="currentPage === pageNumber"
-    >
-      {{ pageNumber }}
-    </button>
+    <div class="button-container">
+      <button
+        class="button"
+        v-for="pageNumber in totalPages"
+        :key="pageNumber"
+        @click="fetchEmployees(pageNumber)"
+        :disabled="currentPage === pageNumber"
+      >
+        {{ pageNumber }}
+      </button>
+    </div>
   </div>
 </template>
 
@@ -60,3 +64,37 @@ export default {
   }
 }
 </script>
+
+<style>
+.grid-container {
+  margin-top: 10vh;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  grid-gap: 10px;
+}
+
+.grid-content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+}
+
+.button-container {
+  display: flex;
+  justify-content: center;
+  margin-top: 20px;
+}
+
+.button {
+  margin: 5px;
+}
+
+.img {
+  border-radius: 50%;
+}
+
+.link {
+  color: black;
+}
+</style>
